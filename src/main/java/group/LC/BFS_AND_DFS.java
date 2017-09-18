9c42904fc18555377a01043b4082f92325c7fa06
abprash <panuradh@buffalo.edu>
2017-09-18 12:21:48 -0400

    added bfs and dfs

---
+++ src/main/java/group/LC/BFS_AND_DFS.java
@@ -0,0 +1,37 @@
+package group.LC;
+
+public class BFS_AND_DFS {
+	
+Queue<TreeNode> q = (Queue<TreeNode>) new LinkedList<TreeNode>();
+Stack<TreeNode> stack = new Stack<TreeNode>();
+
+    public void Bfs(TreeNode root) {
+        q.add(root);
+        
+        while(q.size() > 0){
+            TreeNode temp = q.remove();
+            System.out.println(temp.val);
+            if(temp.left != null)
+                q.add(temp.left);
+            if(temp.right != null)
+                q.add(temp.right);
+        }
+        return;
+    }
+
+    
+    
+    public void DFS_withStack(TreeNode root) {
+    	stack.push(root);
+        
+        while(stack.size() > 0){
+            TreeNode temp = stack.pop();
+            System.out.println(temp.val);
+            if(temp.left != null)
+            	stack.push(temp.left);
+            if(temp.right != null)
+            	stack.push(temp.right);
+        }
+        return;
+    }
+}
