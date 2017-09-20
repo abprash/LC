2d071850fa312f219df1d79699a3594cb9e8bc4f
abprash <panuradh@buffalo.edu>
2017-09-19 21:15:57 -0400

    added optimized mypow

--- src/main/java/group/LC/MyPow.java
+++ src/main/java/group/LC/MyPow.java
@@ -32,4 +32,34 @@ public class MyPow {
 	        }
 	        return res;
 	    }
+	 
+	 //************Putting the  actual good solution
+	 //pow(x,n)
+	 //OKOK solution
+	 
+	 public double myPow2(double x, double n){
+		 //base case
+		 if(n == 0)	return 1;
+		 if(n%2 == 0)
+			 return myPow2(x,n/2)*myPow2(x,n/2);
+		 else
+			 return x * myPow2(x,n/2)*myPow2(x,n/2);
+		 
+	 }
+	 
+	 //optimized - excellent solution
+	 public double myPow3(double x, int n) {
+	        if( n == 0)
+	       return 1;
+	    double temp = myPow3(x, n/2);       
+	    if (n%2 == 0)
+	        return temp*temp;
+	    else
+	    {
+	        if(n > 0)
+	            return x*temp*temp;
+	        else
+	            return (temp*temp)/x;
+	    }
+	    }
 }
