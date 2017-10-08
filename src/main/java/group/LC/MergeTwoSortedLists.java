835a495f51b276dfe849eb5a705d3079fe213059
abprash <panuradh@buffalo.edu>
2017-10-08 17:52:14 -0400

    added recusive - merge two lists

--- src/main/java/group/LC/MergeTwoSortedLists.java
+++ src/main/java/group/LC/MergeTwoSortedLists.java
@@ -1,19 +1,20 @@
 package group.LC;
 
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+ * Definition for singly-linked list.*/
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+ 
 // very inefficient solution though
 //sucks!!!!!!!!!!!!!!
+//scroll down for a much more awesome solution
 
 public class MergeTwoSortedLists {
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
         if(l1 == null || l2 == null){
             if(l1 != null)
                 return l1;
@@ -64,5 +65,36 @@ public class MergeTwoSortedLists {
             temp1 = temp1.next;
         }
         return head;
+    }
+
+
+
+//*************************************************************************************
+
+//short and sweet recursive solution
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        if(l1 == null)  return l2;
+        else if(l2 == null)  return l1;
+        else if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            //return l1;
+            head = l1;
+        }
+        else{
+            l2.next = mergeTwoLists(l1, l2.next);
+            //return l2;
+            head = l2;
+        }
+        return head;
+        
     }
 }
