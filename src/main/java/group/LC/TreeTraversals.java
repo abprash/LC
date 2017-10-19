688718f10500be56c0ee7b5b4d3ae342af5801f1
abprash <panuradh@buffalo.edu>
2017-10-19 17:28:49 -0400

    traversals

--- src/main/java/group/LC/TreeTraversals.java
+++ src/main/java/group/LC/TreeTraversals.java
@@ -84,5 +84,97 @@ public class TreeTraversals {
 	            return false;
 	        return isComplete(root.left) && isComplete(root.right);
 	    }
+	 
+	 //*************************PRE
+	 
+	 public void preOrderTraversal(TreeNode root){
+	        
+	        //iterative
+	        if(root == null)
+	            return;
+	        Stack<TreeNode> stack = new Stack<>();
+	        stack.push(root);
+	        while(!stack.isEmpty()){
+	            TreeNode temp = stack.pop();
+	            //System.out.println(stack);
+	            System.out.println(temp.val);
+	            if(temp.right != null)
+	                stack.push(temp.right);
+	            if(temp.left != null)
+	                stack.push(temp.left);
+	            
+	        }
+	        return;
+	    }
 
+	 //*********************************POST
+	 
+	 
+	 public List<Integer> postorderTraversal(TreeNode root) {
+		    List<Integer> res = new ArrayList<Integer>();
+		 
+		    if(root==null) {
+		        return res;
+		    }
+		 
+		    Stack<TreeNode> stack = new Stack<TreeNode>();
+		    stack.push(root);
+		 
+		    while(!stack.isEmpty()) {
+		        TreeNode temp = stack.peek();
+		        if(temp.left==null && temp.right==null) {
+		            TreeNode pop = stack.pop();
+		            res.add(pop.val);
+		        }
+		        else {
+		            if(temp.right!=null) {
+		                stack.push(temp.right);
+		                temp.right = null;
+		            }
+		 
+		            if(temp.left!=null) {
+		                stack.push(temp.left);
+		                temp.left = null;
+		            }
+		        }
+		    }
+		 
+		    return res;
+		}
+	 
+	 //**************************INORDER
+	 
+	 public List<Integer> inorderTraversal2(TreeNode root) {
+		    List<Integer> result = new ArrayList<Integer>();
+		    if(root==null)
+		        return result;
+		    Stack<TreeNode> stack = new Stack<TreeNode>();
+		    stack.push(root);
+		 
+		    while(!stack.isEmpty()){
+
+		        TreeNode top = stack.peek();
+		        System.out.println(">>"+top.val);
+		        if(top.left!=null){
+		            //first we need to push all the left nodes 
+		            //go until we are all the way down
+		            stack.push(top.left);
+		            top.left=null;
+		        }else{
+		            
+		            //this is where we are printing them in order
+		            result.add(top.val);
+		            
+		            //we are evacuating the left node from the stack
+		            stack.pop();
+		            
+		            //but top still points to the parent - so it will be printed next, but the right child will be going into the stack, so it will be printed afer the parent
+		            if(top.right!=null){
+		                stack.push(top.right);
+		            }
+		        }
+		    }
+		 
+		    return result;
+		}
 }
