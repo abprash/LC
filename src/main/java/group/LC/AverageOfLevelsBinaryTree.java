d5e7055c73ba3aab1a40052029915877d51d8bb7
abprash <panuradh@buffalo.edu>
2017-11-01 14:50:59 -0400

    added avg of levels bin tree

---
+++ src/main/java/group/LC/AverageOfLevelsBinaryTree.java
@@ -0,0 +1,47 @@
+package group.LC;
+
+public class AverageOfLevelsBinaryTree {
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        if(root == null)
+            return new ArrayList<Double>();
+        
+        //lets do a bfs
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        List<Double> ans = new ArrayList<Double>();
+        while(!q.isEmpty()){
+            //get the size of the queue
+            int size = q.size();
+            double sum = 0;
+            //List<Integer> temp = new ArrayList<>();
+            for(int i=0; i<size; i++){
+                //start doing the navigation
+                TreeNode cur = q.poll();
+                if(cur.left != null)
+                    q.offer(cur.left);
+                sum += cur.val;
+                if(cur.right != null)
+                    q.offer(cur.right);
+            }
+            //now calc the avg and add
+            double avg = sum / size;
+            ans.add(avg);
+        }
+        
+        return ans;
+    }
+}
