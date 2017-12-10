890e73b5dcfa2bd6eb64d32c6fb4dca3e5c08e87
abprash <panuradh@buffalo.edu>
2017-12-10 18:37:48 -0500

    added graph valid tree

--- src/main/java/group/LC2/GraphValidTree.java
+++ src/main/java/group/LC2/GraphValidTree.java
@@ -3,7 +3,45 @@ package group.LC2;
 public class GraphValidTree {
 
 }
-
+//ACCEPTED SOLUTION
+//****************************************************
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        //visited array - ints
+        int[] visited = new int[n];
+        //construct the adjacency list for the graph
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i=0; i<n; ++i) { adjList.add(new ArrayList<Integer>()); }
+        for (int[] edge: edges) {
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
+        }
+        //if has cycle, will return false
+        if (hasCycle(-1, 0, visited, adjList)) { 
+            return false; 
+        }  
+        // not 1 single connected component
+        for (int v: visited) { 
+            if (v == 0)
+                return false;  
+        }  
+        return true;
+    }
+    
+    private boolean hasCycle(int pred, int vertex, int[] visited, List<List<Integer>> adjList) {
+        visited[vertex] = 1;  // current vertex is being visited
+        for (Integer succ: adjList.get(vertex)) {  // successors of current vertex
+            if (succ != pred) {  // exclude current vertex's predecessor
+                if (visited[succ] == 1) { return true; }  // back edge/loop detected!
+                else if (visited[succ] == 0) {
+                    if (hasCycle(vertex, succ, visited, adjList)) { return true; }
+                }
+            }
+        }
+        visited[vertex] = 2;
+        return false;
+    }
+}
 
 
 //*********************************
