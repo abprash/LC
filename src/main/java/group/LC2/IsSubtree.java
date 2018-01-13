348e77d7c629a22b925ffc541850c45a76dccc00
abprash <panuradh@buffalo.edu>
2018-01-12 22:41:35 -0500

    more tr

---
+++ src/main/java/group/LC2/IsSubtree.java
@@ -0,0 +1,35 @@
+package group.LC2;
+
+public class IsSubtree {
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(s == null && t == null)
+            return true;
+        if(s == null || t == null)
+            return false;
+        if(helper(s, t))
+            return true;
+        else
+            return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    public boolean helper(TreeNode s, TreeNode t){
+        if(s == null && t == null)
+            return true;
+        if(s == null || t == null)
+            return false;
+        if(s.val != t.val)
+            return false;
+        return helper(s.left, t.left) && helper(s.right, t.right);
+    }
+}
