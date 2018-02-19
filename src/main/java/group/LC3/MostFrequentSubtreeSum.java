3c0bca4103ba94b0083a58979a24f17578fbb2e3
abprash <panuradh@buffalo.edu>
2018-02-19 16:41:53 -0500

    more tr

--- src/main/java/group/LC3/MostFrequentSubtreeSum.java
+++ src/main/java/group/LC3/MostFrequentSubtreeSum.java
@@ -2,6 +2,71 @@ package group.LC3;
 
 public class MostFrequentSubtreeSum {
 
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+	
+	//much better AC solution
+	//my own
+	
+    int[] ans;
+    //have a hashmap for <sum, count>
+    Map<Integer, Integer> map = new HashMap<>();
+    int maxCount = 0;
+    public int[] findFrequentTreeSum(TreeNode root) {
+        
+        if(root == null)
+            return new int[0];
+        
+        helper(root);
+        
+        //get the max counts
+        int len = 0;
+        
+        List<Integer> list = new ArrayList();
+        for(Map.Entry<Integer, Integer> e : map.entrySet()){
+            if(e.getValue() == maxCount)
+                list.add(e.getKey());
+        }
+        
+        //conv the list to an array and return
+        ans = new int[list.size()];
+        for(int i =0; i<list.size(); i++)
+            ans[i] = list.get(i);
+        
+        return ans;
+    }
+    
+    public void helper(TreeNode root){
+        if(root == null)
+            return;
+        //now start finding the sum everytime
+        int sum = root.val + sum(root.left) + sum(root.right);
+        if(!map.containsKey(sum)){
+            maxCount = Math.max(maxCount, 1);
+            map.put(sum,1);
+        }
+        else{
+            maxCount = Math.max(map.get(sum)+1, maxCount);
+            map.put(sum, map.get(sum)+1);
+        }
+        //go further down
+        helper(root.left);
+        helper(root.right);
+    }
+    
+    public int sum(TreeNode root){
+        if(root == null)
+            return 0;
+        return root.val + sum(root.left) + sum(root.right);
+    }
 }
 /**
  * Definition for a binary tree node.
