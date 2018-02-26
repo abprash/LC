a2034780a915c47f7247e7e347b9aceff30308c1
abprash <panuradh@buffalo.edu>
2018-02-26 13:39:04 -0500

    more mth

--- src/main/java/group/LC3/CountPrimes.java
+++ src/main/java/group/LC3/CountPrimes.java
@@ -1,9 +1,50 @@
 package group.LC3;
 
+import java.util.*;
+
+//faster solution, but still not AC
+
 public class CountPrimes {
 
-}
 
+    
+    Map<Integer, Integer> map = new HashMap<>();
+    public int countPrimes(int n) {
+        
+        map.put(1, 0);
+        map.put(2, 0);
+        map.put(3, 1);
+        map.put(4, 2);
+        
+        //check the map
+        ///lets try a recursive function
+        if(map.containsKey(n))
+            return map.get(n);
+        for(int i=5; i<=n; i++){
+            int res = isPrime(i-1) + map.get(i-1);
+            map.put(i, res);
+            //System.out.println(map);
+        }
+        //System.out.println(map);
+        return map.get(n);
+    }
+    
+    public int isPrime(int n){
+        boolean isComposite = false;
+        for(int i=2; i<= (int) Math.sqrt(n); i++){
+            if(isComposite)
+                return 0;
+            if(n%i == 0){
+                isComposite = true;
+                break;
+            }
+        }
+        if(!isComposite)
+            return 1;
+        else
+            return 0;
+    }
+}
 
 //Algo is right
 //brute force algo - TLE
