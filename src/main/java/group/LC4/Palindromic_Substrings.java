ce725fa0060c6cad0cd560b4fb3c919150922324
abprash <panuradh@buffalo.edu>
2018-08-11 15:38:22 -0700

    Systematic medium problems

---
+++ src/main/java/group/LC4/Palindromic_Substrings.java
@@ -0,0 +1,52 @@
+package group.LC4;
+
+public class Palindromic_Substrings {
+	
+	class Solution {
+	    public int countSubstrings(String s) {
+	        if(s == null || s.length() == 0)
+	            return 0;
+	        int ans = 0;
+	        
+	        for(int i=0; i<s.length(); i++){
+	            //get the first center character and start expanding on either sides
+	            char c = s.charAt(i);
+	            if(i+1 < s.length()){
+	                char c1 = s.charAt(i), c2 = s.charAt(i+1);
+	                if(c1 == c2)
+	                    ans++;
+	                int l = i-1, r = i+2;
+	                while(c1 == c2 && l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+	                    l--;
+	                    r++;
+	                    ans++;
+	                }
+	            }
+	            ans++; //for the individual character is a palindrome
+	            int l = i-1, r = i+1;
+	            while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+	                l--;
+	                r++;
+	                ans++;
+	            }
+	        }
+	        
+	        //now consider the even length palindromes
+	        // for(int i=0; i<s.length()-1; i++){
+	        //     //we will take i and i+1 as the centers and do the same
+	        //     char c1 = s.charAt(i), c2 = s.charAt(i+1);
+	        //     if(c1 == c2)
+	        //         ans++;
+	        //     int l = i-1, r = i+2;
+	        //     while(c1 == c2 && l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+	        //         l--;
+	        //         r++;
+	        //         ans++;
+	        //     }
+	        // }
+	        
+	        return ans;
+	    }
+	}
+
+}
