f485749c9009ef0afd41fce0c797f7d6e554e544
abprash <panuradh@buffalo.edu>
2018-08-11 19:56:49 -0700

    Kill process

---
+++ src/main/java/group/LC4/KillProcess.java
@@ -0,0 +1,30 @@
+package group.LC4;
+
+public class KillProcess {
+
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        List<Integer> ans = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<pid.size(); i++){
+            int pidEl = pid.get(i);
+            int ppidEl = ppid.get(i);
+            if(!map.containsKey(ppidEl))
+                map.put(ppidEl, new ArrayList<Integer>());
+            map.get(ppidEl).add(pidEl);
+        }
+        stack.add(kill);
+        while(! stack.isEmpty()){
+            int toKill = stack.pop();
+            visited.add(toKill);
+            set.add(toKill);
+            if(map.containsKey(toKill)){
+                set.addAll(map.get(toKill));
+                stack.addAll(map.get(toKill));
+            }
+        }
+        ans.addAll(set);
+        return ans;
+    }
+}
