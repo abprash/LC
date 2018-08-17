c9c0fbc4dedc1b8d7601e88be96b798ca9de7d87
abprash <panuradh@buffalo.edu>
2018-08-17 00:32:29 -0700

    More changes.

--- src/main/java/not_understanding/SplitBST.java
+++ src/main/java/not_understanding/SplitBST.java
@@ -1,4 +1,4 @@
-package group.LC4;
+package not_understanding;
 
 //https://leetcode.com/problems/split-bst/description/
 
@@ -34,4 +34,52 @@ public class SplitBST {
         return res;
         
     }
+    
+    /////////////////////////////////
+    
+    
+    public TreeNode splitBST2(TreeNode root, int V){
+    	TreeNode[] res = new TreeNode[2];
+    	if(root == null){
+    		return res;
+    	}
+    	if(root.val <= V){
+    		res[0] = root;
+    		//split in the right
+    		TreeNode[] temp = splitBST2(root.right, V);
+    		root.right = temp[0];
+    		res[1] = temp[1];
+    	}
+    	else{
+    		//we will go left
+    		//so, the second element in res will be greater
+    		res[1] = root;
+    		TreeNode[] temp = splitBST2(root.left, V);
+    		root.left = temp[1];
+    		res[0] = temp[0];
+    	}
+    	return res;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
