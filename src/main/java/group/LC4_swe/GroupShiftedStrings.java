5be31628774f167783c991226d9cbba2c3bc16f0
abprash <panuradh@buffalo.edu>
2018-08-25 19:07:03 -0700

    Added Group shifting strings. interesting problem.

--- src/main/java/group/LC4_swe/GroupShiftedStrings.java
+++ src/main/java/group/LC4_swe/GroupShiftedStrings.java
@@ -7,42 +7,127 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class GroupShiftedStrings {
 
+//the below solution works for all input with duplicates also . YAYYYY!! 
 
+class Solution {
     public List<List<String>> groupStrings(String[] strings) {
         List<List<String>> ans = new ArrayList<>();
         if(strings == null || strings.length == 0)
             return ans;
-        Map<Integer, Set<String>> map = new HashMap<>();
-        
+        Map<String, Integer> map = new HashMap<>();
+        Set<String> visited = new HashSet<>();
+        //add the given list to a set. for faster lookups.
         for(String s : strings){
-            if(!map.containsKey(s.length()))
-                map.put(s.length(), new HashSet<String>());
-            map.get(s.length()).add(s);
+            if(!map.containsKey(s))
+                map.put(s,1);
+            else
+                map.put(s,map.get(s)+1);
         }
-        for(Map.Entry<Integer, Set<String>> entry : map.entrySet()){
-            Set<String> clone = new HashSet(entry.getValue());
-            Set<String> set = entry.getValue();
-            for(String word : clone){
-                if(!set.contains(word))
-                    continue;
-                List<String> temp = new ArrayList<>();
-                temp.add(word);
-                char[] arr = word.toCharArray();
-                for(int i=0; i<arr.length; i++){
-                    for(int j=1; j<26; j++){
-                        //start adding the nos.
-                        char newChar = (char) (arr[i] + j > 'z' ? (arr[i] + j)%26 : arr[i] + j);
-                        arr[i] = newChar;
+        // System.out.println(map);
+        for(String s : strings){
+            //check if this word has already been done
+            if(visited.contains(s)){
+                continue;
+            }
+            List<String> current = new ArrayList<>();
+            current.add(s);
+            map.put(s, map.get(s)-1);//deduct this occurrence
+            //handle the possible duplicates
+            while(map.get(s) > 0){
+                current.add(s);
+                map.put(s, map.get(s)-1);//decrement the occurrence in map    
+            }
+            visited.add(s);
+            //else, start shifting around
+            List<String> shiftList = shift(s);
+            //check if any of the strings are present in the set
+            for(String shiftedString : shiftList){
+                if(map.containsKey(shiftedString) && !shiftedString.equals(s)){
+                    //first time deduction
+                    if(map.containsKey(shiftedString))
+                        map.put(shiftedString, map.get(shiftedString)-1);
+                    current.add(shiftedString);//add to the answer list
+                    visited.add(shiftedString);//add to the lookup list, so that we don't repeat the check again
+                    //handle the duplicates now
+                    while(map.containsKey(shiftedString) && map.get(shiftedString) > 0){
+                        current.add(shiftedString);
+                        map.put(shiftedString, map.get(shiftedString)-1);//decrement the occurrence in map    
                     }
                 }
-                if(set.contains(String.valueOf(arr))){
-                    temp.add(String.valueOf(arr));
-                    set.remove(String.valueOf(arr));
-                }
             }
+            ans.add(current);
         }
         return ans;
     }
+    
+    public List<String> shift(String s){
+        //only lower case alphabets are present
+        //start shifting around
+        List<String> shifted = new ArrayList<>();
+        char[] charString = s.toCharArray();
+        for(int i=1; i<=26; i++){
+            for(int j=0; j<charString.length; j++){
+                charString[j]++;
+                if(charString[j] > 'z')
+                    charString[j] = (char) ('z' - charString[j] + 'a' + 1);
+            }
+            shifted.add(String.valueOf(charString));
+        }
+        // System.out.println(shifted);
+        return shifted;
+    }
+}
+
+//the below solution works for input with no duplicates. but fails for ones with duplicate entries
+
+public class GroupShiftedStrings {
+	
+	    public List<List<String>> groupStrings(String[] strings) {
+	        List<List<String>> ans = new ArrayList<>();
+	        if(strings == null || strings.length == 0)
+	            return ans;
+	        Set<String> set = new HashSet<>();
+	        Set<String> visited = new HashSet<>();
+	        //add the given list to a set. for faster lookups.
+	        for(String s : strings)
+	            set.add(s);
+	        for(String s : strings){
+	            //check if this word has already been done
+	            if(visited.contains(s))
+	                continue;
+	            List<String> current = new ArrayList<>();
+	            current.add(s);
+	            visited.add(s);
+	            //else, start shifting around
+	            List<String> shiftList = shift(s);
+	            //check if any of the strings are present in the set
+	            for(String shiftedString : shiftList){
+	                if(set.contains(shiftedString) && !shiftedString.equals(s)){
+	                    current.add(shiftedString);//add to the answer list
+	                    visited.add(shiftedString);//add to the lookup list, so that we don't repeat the check again
+	                }
+	            }
+	            ans.add(current);
+	        }
+	        return ans;
+	    }
+	    
+	    public List<String> shift(String s){
+	        //only lower case alphabets are present
+	        //start shifting around
+	        List<String> shifted = new ArrayList<>();
+	        char[] charString = s.toCharArray();
+	        for(int i=1; i<=26; i++){
+	            for(int j=0; j<charString.length; j++){
+	                charString[j]++;
+	                if(charString[j] > 'z')
+	                    charString[j] = (char) ('z' - charString[j] + 'a' + 1);
+	            }
+	            shifted.add(String.valueOf(charString));
+	        }
+	        // System.out.println(shifted);
+	        return shifted;
+	    }
+	
 }
